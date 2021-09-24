0398aae0cc8a6d167980999c3e00e10ea4d891e2
Nino Filiu <nino.filiu@gmail.com>
2021-09-24 14:34:14 +0200

    new blink alg

--- julie/dev.c
+++ julie/dev.c
@@ -2,15 +2,77 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main() {
-  printf("\n");
-  for (int i = 0; i < 100; i++) {
-    printf("\e[F");
-    int on = rand() % 2;
-    printf(on ? "XXXXXXXX" : "........");
+void set_light(int on) {
+  int width = 50;
+  int height = 25;
+  char* s = on ? "X" : "_";
+  for (int line = 0; line < height; line++) {
+    for (int column = 0; column < width; column++) {
+      printf("%s", s);
+    }
     printf("\n");
-    usleep(50 * 1000);
   }
-  printf("\n");
-  return 0;
+  for (int line = 0; line < height; line++) {
+    printf("\e[F");
+  }
+}
+
+float rand_float_range(float from, float to) {
+  float scale = rand() / (float)RAND_MAX;
+  return from + scale * (to-from);
+}
+
+float rand_int_range(int min, int max) {
+  return min + rand()%(max-min);
+}
+
+void stays_on() {
+  float min_duration = 1.0;
+  float max_duration = 5.0;
+
+  float duration = rand_float_range(min_duration, max_duration);
+  set_light(1);
+  usleep((int)(duration*1000*1000));
+}
+
+void stays_off() {
+  float min_duration = 1.0;
+  float max_duration = 5.0;
+
+  float duration = rand_float_range(min_duration, max_duration);
+  set_light(0);
+  usleep((int)(duration*1000*1000));
+}
+
+void flickers() {
+  float total_min_duration = 0.0;
+  float total_max_duration = 5.0;
+  float flicker_duration = 1.0 / 20;
+
+  float total_duration = rand_float_range(total_min_duration, total_max_duration);
+  for (int i = 0; i < (int)(total_duration/flicker_duration); i++) {
+    set_light(rand() % 2);
+    usleep((int)(flicker_duration*1000*1000));
+  }
+}
+
+void setup() {}
+
+void loop() {
+  int min_nb_flickers = 1;
+  int max_nb_flickers = 5;
+
+  int nb_flickers = rand_int_range(min_nb_flickers, max_nb_flickers);
+  stays_on();
+  for (int i = 0; i < nb_flickers; i++) {
+    flickers();
+    stays_off();
+  }
+}
+
+void main() {
+  setup();
+  while(1) {
+    loop();
+  }
 }
