3c16414e8d9f9053fad32678ae3b58ac6d54da11
Nino Filiu <nino.filiu@gmail.com>
2022-03-23 23:37:17 +0100

    draw invaders

--- 50N1A/Spielplatz/Spielplatz.ino
+++ 50N1A/Spielplatz/Spielplatz.ino
@@ -1,18 +1,36 @@
-const int leftButton = 4;
-const int rightButton = 2;
+#include "U8glib.h"
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(leftButton, INPUT);
-  pinMode(rightButton, INPUT);
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);
+
+uint8_t invaderBitmap[8] = {
+    0b00011000, 0b00111100, 0b01111110, 0b01011010,
+    0b01011010, 0b11111111, 0b10100101, 0b10100101,
+};
+
+void drawInvader(uint8_t x, uint8_t y) {
+  u8g.drawBitmap(x, y, 1, 8, invaderBitmap);
 }
 
-void loop() {
-  int leftPressed = digitalRead(leftButton);
-  int rightPressed = digitalRead(rightButton);
-  int xShift = 0;
-  if (leftPressed == HIGH) { xShift--; }
-  if (rightPressed == HIGH) { xShift++; }
-  Serial.println(xShift);
-  delay(40); // ~24fps
+void draw(void) {
+  for (int x = 0; x < 128; x += 16) {
+    for (int y = 0; y < 64; y += 16) {
+      drawInvader(x, y);
+    }
+  }
+}
+
+void setup(void) {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+}
+
+void loop(void) {
+  u8g.firstPage();
+  do {
+    u8g.setFont(u8g_font_6x10);
+    u8g.setFontRefHeightExtendedText();
+    u8g.setDefaultForegroundColor();
+    u8g.setFontPosTop();
+    draw();
+  } while (u8g.nextPage());
 }
