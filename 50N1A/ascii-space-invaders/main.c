5f89e7a22b06698f7b57ba46c2e41cdf4c468b2a
Nino Filiu <nino.filiu@gmail.com>
2022-03-23 18:05:45 +0100

    player

--- 50N1A/ascii-space-invaders/main.c
+++ 50N1A/ascii-space-invaders/main.c
@@ -1,37 +1,52 @@
 #include <stdio.h>
+#include <unistd.h>
 
 const int width = 32;
 const int height = 8;
 
-void draw(char screen[]) {
-  printf("\033[0;0H");
-  putchar('+');
+struct Player {
+  int x;
+  int lives;
+};
+
+void draw(struct Player player) {
+  char screen[width * height];
+
+  for (int i = 0; i < width * height; i++) {
+    screen[i] = ' ';
+  }
+
+  screen[width * (height - 1) + player.x] = '0' + player.lives;
+
+  printf("\033[0;0H+");
   for (int x = 0; x < width; x++) {
     putchar('-');
   }
-  putchar('+');
-  putchar('\n');
+  printf("+\n");
   for (int y = 0; y < height; y++) {
     putchar('|');
     for (int x = 0; x < width; x++) {
       putchar(screen[width * y + x]);
     }
-    putchar('|');
-    putchar('\n');
+    printf("|\n");
   }
   putchar('+');
   for (int x = 0; x < width; x++) {
     putchar('-');
   }
-  putchar('+');
-  putchar('\n');
+  printf("+\n");
 }
 
 int main() {
-  char screen[width * height];
-  for (int i = 0; i < width * height; i++) {
-    screen[i] = 'x';
+  struct Player player;
+  player.x = 0;
+  player.lives = 5;
+
+  for (int j = 0; j < 1000; j++) {
+    player.x = (player.x + 1) % width;
+    draw(player);
+    usleep((int)(1000 * 1000 / 60));
   }
-  draw(screen);
+
   return 0;
 }
